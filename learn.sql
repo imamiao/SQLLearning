558dc8d9657c4f4bc855c662452576ca5ffe246d
zhaoyiming <zhaoyimingg@gmail.com>
2020-07-06 08:14:37 +0800

    update

--- learn.sql
+++ learn.sql
@@ -2,59 +2,72 @@ CREATE DATABASE product;
 
 CREATE TABLE product
 (
-product_id CHAR(4) NOT NULL ,
-product_name VARCHAR(100) NOT NULL ,
-product_type VARCHAR(32) NOT NULL ,
-sale_price INTEGER ,
-purchase_price INTEGER ,
-regist_date DATE ,
-PRIMARY KEY (product_id)
+    product_id     CHAR(4)      NOT NULL,
+    product_name   VARCHAR(100) NOT NULL,
+    product_type   VARCHAR(32)  NOT NULL,
+    sale_price     INTEGER,
+    purchase_price INTEGER,
+    regist_date    DATE,
+    PRIMARY KEY (product_id)
 );
 
 DROP TABLE product;
-ALTER TABLE product ADD COLUMN product_name_pinyin VARCHAR(100);
-ALTER TABLE product DROP COLUMN product_name_pinyin;
+ALTER TABLE product
+    ADD COLUMN product_name_pinyin VARCHAR(100);
+ALTER TABLE product
+    DROP COLUMN product_name_pinyin;
 
 START TRANSACTION;
-INSERT INTO Product VALUES ('0001', 'T恤' ,'⾐服', 1000, 500, '2009-09-20');
-INSERT INTO Product VALUES ('0002', '打孔器', '办公⽤品', 500, 320, '2009-09-11');
-INSERT INTO Product VALUES ('0003', '运动T恤', '⾐服', 4000, 2800, NULL);
-INSERT INTO Product VALUES ('0004', '菜⼑', '厨房⽤具', 3000, 2800, '2009-09-20');
-INSERT INTO Product VALUES ('0005', '⾼压锅', '厨房⽤具', 6800, 5000, '2009-01-15');
-INSERT INTO Product VALUES ('0006', '叉⼦', '厨房⽤具', 500, NULL, '2009-09-20');
-INSERT INTO Product VALUES ('0007', '擦菜板', '厨房⽤具', 880, 790, '2008-04-28');
-INSERT INTO Product VALUES ('0008', '圆珠笔', '办公⽤品', 100, NULL, '2009-11-11');
+INSERT INTO Product
+VALUES ('0001', 'T恤', '⾐服', 1000, 500, '2009-09-20');
+INSERT INTO Product
+VALUES ('0002', '打孔器', '办公⽤品', 500, 320, '2009-09-11');
+INSERT INTO Product
+VALUES ('0003', '运动T恤', '⾐服', 4000, 2800, NULL);
+INSERT INTO Product
+VALUES ('0004', '菜⼑', '厨房⽤具', 3000, 2800, '2009-09-20');
+INSERT INTO Product
+VALUES ('0005', '⾼压锅', '厨房⽤具', 6800, 5000, '2009-01-15');
+INSERT INTO Product
+VALUES ('0006', '叉⼦', '厨房⽤具', 500, NULL, '2009-09-20');
+INSERT INTO Product
+VALUES ('0007', '擦菜板', '厨房⽤具', 880, 790, '2008-04-28');
+INSERT INTO Product
+VALUES ('0008', '圆珠笔', '办公⽤品', 100, NULL, '2009-11-11');
 COMMIT;
 
-CREATE TABLE addressbook(
-register_no INTEGER primary key ,
-name varchar(128) not null ,
-address varchar(256) not null ,
-tel_no char(10) ,
-mail_address char(20) ,
-postal_code char(8) not null
+CREATE TABLE addressbook
+(
+    register_no  INTEGER primary key,
+    name         varchar(128) not null,
+    address      varchar(256) not null,
+    tel_no       char(10),
+    mail_address char(20),
+    postal_code  char(8)      not null
 );
 
-ALTER TABLE addressbook ADD COLUMN postal_code char(8) not null ;
+ALTER TABLE addressbook
+    ADD COLUMN postal_code char(8) not null;
 
 DROP TABLE addressbook;
 
 SELECT product_id, product_name, purchase_price
 FROM product;
 
-SELECT * FROM product;
-
-SELECT product_id AS id,
-product_name AS name,
-purchase_price AS price
+SELECT *
 FROM product;
 
-SELECT product_id AS id,
-product_name AS 名称,
-purchase_price AS 价格
+SELECT product_id     AS id,
+       product_name   AS name,
+       purchase_price AS price
 FROM product;
 
-SELECT '商品' AS string, 38 AS number, '2009-02-24' AS date, product_id, product_name
+SELECT product_id     AS id,
+       product_name   AS 名称,
+       purchase_price AS 价格
+FROM product;
+
+SELECT ' 商品' AS string, 38 AS number, '2009-02-24' AS date, product_id, product_name
 FROM product;
 
 SELECT DISTINCT purchase_price
@@ -75,14 +88,14 @@ WHERE product_type = '⾐服';
 多⾏注释
 多⾏注释
 */
-SELECT product_name,sale_price,sale_price*2 AS "sale_price_x2"
+SELECT product_name, sale_price, sale_price * 2 AS "sale_price_x2"
 FROM product;
 
 SELECT product_name, product_type, sale_price
 FROM product
 WHERE sale_price = 500;
 
-SELECT product_name,product_type,sale_price
+SELECT product_name, product_type, sale_price
 FROM product
 WHERE sale_price <> 500;
 
@@ -98,19 +111,27 @@ SELECT product_name, sale_price, purchase_price
 FROM product
 WHERE sale_price - purchase_price >= 500;
 
-CREATE TABLE chars (
-chr CHAR(3) NOT NULL ,
-primary key (chr)
+CREATE TABLE chars
+(
+    chr CHAR(3) NOT NULL,
+    primary key (chr)
 );
 
 BEGIN TRANSACTION;
-INSERT INTO Chars VALUES ('1');
-INSERT INTO chars VALUES ('a');
-INSERT INTO Chars VALUES ('2');
-INSERT INTO Chars VALUES ('3');
-INSERT INTO Chars VALUES ('10');
-INSERT INTO Chars VALUES ('11');
-INSERT INTO Chars VALUES ('222');
+INSERT INTO Chars
+VALUES ('1');
+INSERT INTO chars
+VALUES ('a');
+INSERT INTO Chars
+VALUES ('2');
+INSERT INTO Chars
+VALUES ('3');
+INSERT INTO Chars
+VALUES ('10');
+INSERT INTO Chars
+VALUES ('11');
+INSERT INTO Chars
+VALUES ('222');
 COMMIT;
 
 SELECT chr
@@ -123,7 +144,7 @@ WHERE purchase_price <> 2800;
 
 SELECT product_name, purchase_price
 FROM product
-WHERE purchase_price IS NULL ;
+WHERE purchase_price IS NULL;
 
 SELECT product_name, product_type, sale_price
 FROM product
@@ -150,22 +171,33 @@ FROM product
 WHERE regist_date >= '2009-4-28';
 
 -- 对null使⽤⽐较运算符，得不到任何值
-SELECT * FROM product WHERE purchase_price = NULL;
-SELECT * FROM product WHERE purchase_price <> NULL;
-SELECT * FROM product WHERE purchase_price > NULL;
+SELECT *
+FROM product
+WHERE purchase_price = NULL;
+SELECT *
+FROM product
+WHERE purchase_price <> NULL;
+SELECT *
+FROM product
+WHERE purchase_price > NULL;
 
-SELECT product_name, sale_price, purchase_price, sale_price-purchase_price AS 差值
+SELECT product_name, sale_price, purchase_price, sale_price - purchase_price AS 差值
 FROM product
 WHERE sale_price - purchase_price >= 500;
 
-SELECT product_name, sale_price, purchase_price, sale_price-purchase_price AS "差值"
+SELECT product_name, sale_price, purchase_price, sale_price - purchase_price AS "差值"
 FROM product
 WHERE NOT sale_price - purchase_price < 500;
 
-SELECT product_name, product_type, sale_price, (sale_price*0.9) AS "0.9sale_price", purchase_price,
-(sale_price*0.9)-purchase_price AS profit
+SELECT product_name,
+       product_type,
+       sale_price,
+       (sale_price * 0.9)                  AS "0.9sale_price",
+       purchase_price,
+       (sale_price * 0.9) - purchase_price AS profit
 FROM product
-WHERE (sale_price*0.9)-purchase_price > 100 AND (product_type IN ('办公⽤品','厨房⽤具'));
+WHERE (sale_price * 0.9) - purchase_price > 100
+  AND (product_type IN ('办公⽤品', '厨房⽤具'));
 
 SELECT count(*)
 FROM product;
@@ -173,10 +205,20 @@ FROM product;
 SELECT count(purchase_price)
 FROM product;
 
-SELECT sum(sale_price),avg(sale_price),max(sale_price),sum(purchase_price),avg(purchase_price),max(purchase_price)
+SELECT sum(sale_price),
+       avg(sale_price),
+       max(sale_price),
+       sum(purchase_price),
+       avg(purchase_price),
+       max(purchase_price)
 FROM product;
 
-SELECT sum(DISTINCT sale_price),avg( DISTINCT sale_price),max(sale_price),sum(purchase_price),avg(purchase_price),max(purchase_price)
+SELECT sum(DISTINCT sale_price),
+       avg(DISTINCT sale_price),
+       max(sale_price),
+       sum(purchase_price),
+       avg(purchase_price),
+       max(purchase_price)
 FROM product;
 
 SELECT count(DISTINCT product_type)
@@ -188,11 +230,11 @@ FROM product;
 SELECT sum(sale_price), sum(DISTINCT sale_price)
 FROM product;
 
-SELECT product_type,regist_date,count(*)
+SELECT product_type, regist_date, count(*)
 FROM product
-GROUP BY product_type,regist_date;
+GROUP BY product_type, regist_date;
 
-SELECT purchase_price,count(*)
+SELECT purchase_price, count(*)
 FROM product
 GROUP BY purchase_price;
 
@@ -214,14 +256,14 @@ HAVING count(*) = 2;
 
 SELECT product_type, avg(sale_price)
 FROM product
-WHERE 1=1
+WHERE 1 = 1
 GROUP BY product_type
 HAVING avg(sale_price) >= 0
 ORDER BY avg(sale_price);
 
 SELECT *
 FROM product
-ORDER BY sale_price ASC,product_id ASC;
+ORDER BY sale_price ASC, product_id ASC;
 
 -- PostgreSQL会把NULL当成max来处理
 -- FROM 》 WHERE 》 GROUP BY 》 HAVING 》 SELECT 》 ORDER BY
@@ -233,44 +275,78 @@ ORDER BY sp, id;
 -- 2代指name,1代指id。标号是select中列出来的列的顺序。淦！还能这样
 SELECT product_id AS id, product_name AS name, sale_price AS sp, purchase_price
 FROM product
-ORDER BY 2,1 ;
+ORDER BY 2, 1;
 
 SELECT product_type, SUM(sale_price)
 FROM Product
 WHERE regist_date > '2009-09-01'
 GROUP BY product_type;
 
-SELECT product_type,sum(sale_price) AS sumSP,sum(purchase_price) AS sumPP
+SELECT product_type, sum(sale_price) AS sumSP, sum(purchase_price) AS sumPP
 FROM product
 --WHERE purchase_price * 1.5 > sale_price
 GROUP BY product_type
 HAVING sum(purchase_price) * 1.5 = sum(sale_price);
 
-SELECT product_id,product_name,product_type,sale_price,purchase_price,regist_date
+SELECT product_id, product_name, product_type, sale_price, purchase_price, regist_date
 FROM product
-ORDER BY regist_date DESC ,sale_price;
+ORDER BY regist_date DESC, sale_price;
 
 -- page 116
 
+CREATE TABLE productins
+(product_id CHAR(4) NOT NULL primary key ,
+ product_name VARCHAR(100) NOT NULL,
+ product_type VARCHAR(32) NOT NULL,
+ sale_price INTEGER DEFAULT 0,
+ purchase_price INTEGER,
+ regist_date DATE
+);
 
+INSERT INTO productins (product_id, product_name, product_type, sale_price, purchase_price, regist_date) VALUES ('0001','T恤衫','衣服',1000,500,'2009-09-20');
 
+INSERT INTO productins VALUES ('0002','T恤衫','衣服',1000,500,'2009-09-20');
 
+INSERT INTO productins VALUES ('0003','T恤衫','衣服',1000,NULL,'2009-09-20');
 
+INSERT INTO productins (product_id, product_name, product_type, sale_price, purchase_price, regist_date) VALUES ('0004','T恤衫','衣服',DEFAULT,500,'2009-09-20');
 
+SELECT * FROM productins WHERE product_id = '0004';
 
+INSERT INTO productins (product_id, product_name, product_type, purchase_price, regist_date) VALUES ('0005','T恤衫','衣服',500,'2009-09-20');
 
+CREATE TABLE  productcopy
+(
+    product_id CHAR(4) NOT NULL primary key ,
+    product_name VARCHAR(100) NOT NULL ,
+    product_type VARCHAR(32) NOT NULL ,
+    sale_price INTEGER ,
+    purchase_price INTEGER ,
+    regist_date DATE
+);
 
+INSERT INTO productcopy (product_id, product_name, product_type, sale_price, purchase_price, regist_date)
+SELECT product_id, product_name, product_type, sale_price, purchase_price,regist_date
+FROM product;
 
+CREATE TABLE producttype
+(
+    product_type VARCHAR(32) NOT NULL primary key ,
+    sum_sale_price INTEGER ,
+    sum_purchase_price INTEGER
+)
 
+INSERT INTO producttype (product_type, sum_sale_price, sum_purchase_price)
+SELECT product_type,SUM(sale_price),SUM(purchase_price)
+FROM product
+GROUP BY product_type;
 
+DELETE FROM product
+WHERE sale_price >= 4000;
 
+SELECT * FROM product;
 
-
-
-
-
-
-
+-- page 133
 
 
 
